d576571283b24411e89a9b8356fb603714187a18
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-01-31 21:03:33 -0200

    Initial Commit

---
+++ Gradient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gradient
+//
+//  Created by Marcelo Sampaio on 1/31/15.
+//  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
